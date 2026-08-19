e20dcf7152168d8ae4d980a36b29028a9ef9ee0f
Matheus <paschuiniomatheus@gmail.com>
2026-08-19 19:25:19 -0300

    aula: 19-08-26

--- ToDo/Backend/index.js
+++ ToDo/Backend/index.js
@@ -11,7 +11,7 @@ const app = new express();
 app.use(express.json());
 app.use(cors({
     credential: true,
-    origin: "http://localhost:3000"
+    origin: "http://localhost:5173"
 }));
 // obrigatoriamente o swagger deve vir antes das rotas
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
